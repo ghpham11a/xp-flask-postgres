eac8b503980ee64dcf8db8049e5fa15d9aaa65ba
Anthony Pham <gm.pham@gmail.com>
2025-02-28 16:39:08 -0500

    add splunk

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,6 @@ from flask import Flask
 from app.extensions import init_middleware, init_pg, init_redis, init_kafka_producer, init_test_kafka_consumer
 from app.routes import orders
 from app.config import Config
-import requests
-import json
 
 def create_app(config_object=Config):
     app = Flask(__name__)
@@ -14,7 +12,7 @@ def create_app(config_object=Config):
     pg_pool = init_pg(app)
     redis_client = init_redis(app)
     kafka_producer = init_kafka_producer(app)
-    # init_test_kafka_consumer(app)
+    init_test_kafka_consumer(app)
 
     app.extensions["PG_POOL"] = pg_pool
     app.extensions["REDIS_CLIENT"] = redis_client
@@ -25,36 +23,3 @@ def create_app(config_object=Config):
     app.wsgi_app = init_middleware(app.wsgi_app)
 
     return app
-
-def test_splunk(app):
-    # 2. Your HEC token.
-    splunk_hec_url = app.config["HEC_URL"]
-    splunk_token = app.config["HEC_TOKEN"]
-
-    print("splunk_hec_url", splunk_hec_url, "splunk_token", splunk_token)
-
-    app.config
-
-    # 3. Set the headers with Splunk authorization.
-    headers = {
-        "Authorization": f"Splunk {splunk_token}",
-        "Content-Type": "application/json"
-    }
-
-    payload = {
-        "event": {
-            "message": "Hello from inside Kubernetes!",
-            "extra_info": "This is a sample Python POST request"
-        }
-    }
-
-    try:
-        # 5. Make the POST request to Splunk HEC.
-        response = requests.post(splunk_hec_url, headers=headers, data=json.dumps(payload), timeout=5)
-
-        # 6. Print the status and response.
-        print("Status Code:", response.status_code)
-        print("Response Body:", response.text)
-
-    except requests.exceptions.RequestException as e:
-        print("Error sending POST request:", str(e))
